371a6f38066ce70fadd7b8de0db530f5abc5f91c
niiyy <smailaberkaoui@gmail.com>
2022-08-06 20:34:14 +0200

    dev file

--- sidebars.js
+++ sidebars.js
@@ -6,6 +6,11 @@ module.exports = {
       label: 'Installation',
     },
     {
+      type: 'doc',
+      id: 'nx/dev/dev',
+      label: 'Developpement',
+    },
+    {
       type: 'category',
       label: 'Client',
       items: [
